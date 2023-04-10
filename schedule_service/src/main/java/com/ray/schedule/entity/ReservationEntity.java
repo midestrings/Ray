8c101a346bca6c8064b58540c45bf6f0cbbfb1b5
midestrings <ayomideyerokun@gmail.com>
2023-04-10 20:17:36 +0100

    create schedule proto

--- schedule_service/src/main/java/com/ray/schedule/entity/ReservationEntity.java
+++ schedule_service/src/main/java/com/ray/schedule/entity/ReservationEntity.java
@@ -1,14 +1,209 @@
 package com.ray.schedule.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity(name = "Reservation")
 public class ReservationEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column
     private String type;
+    @Column
+    private int vehicleId;
+    @Column
+    private String clientEmail;
+    @Column
+    private String clientName;
+    @Column
+    private String pickupAddress;
+    @Column
+    private String destinationAddress;
+    @Column
+    private Date pickupTime;
+    @Column
+    private Date dropOffTime;
+    @Column
+    private String dropOffAddress;
+    @Column
+    private Date expectedEndTime;
+    @Column
+    private double pickupLatitude;
+    @Column
+    private double pickupLongitude;
+    @Column
+    private double dropOffLatitude;
+    @Column
+    private double dropOffLongitude;
+    @Column
+    private double destinationLatitude;
+    @Column
+    private double destinationLongitude;
+    @Column
+    private String status;
+    @Column
+    private double rating;
+    @Column
+    private String vehiclePlateNo;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(int vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getClientEmail() {
+        return clientEmail;
+    }
+
+    public void setClientEmail(String clientEmail) {
+        this.clientEmail = clientEmail;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getPickupAddress() {
+        return pickupAddress;
+    }
+
+    public void setPickupAddress(String pickupAddress) {
+        this.pickupAddress = pickupAddress;
+    }
+
+    public String getDestinationAddress() {
+        return destinationAddress;
+    }
+
+    public void setDestinationAddress(String destinationAddress) {
+        this.destinationAddress = destinationAddress;
+    }
+
+    public Date getPickupTime() {
+        return pickupTime;
+    }
+
+    public void setPickupTime(Date pickupTime) {
+        this.pickupTime = pickupTime;
+    }
+
+    public Date getDropOffTime() {
+        return dropOffTime;
+    }
+
+    public void setDropOffTime(Date dropOffTime) {
+        this.dropOffTime = dropOffTime;
+    }
+
+    public String getDropOffAddress() {
+        return dropOffAddress;
+    }
+
+    public void setDropOffAddress(String dropOffAddress) {
+        this.dropOffAddress = dropOffAddress;
+    }
+
+    public Date getExpectedEndTime() {
+        return expectedEndTime;
+    }
+
+    public void setExpectedEndTime(Date expectedEndTime) {
+        this.expectedEndTime = expectedEndTime;
+    }
+
+    public double getPickupLatitude() {
+        return pickupLatitude;
+    }
+
+    public void setPickupLatitude(double pickupLatitude) {
+        this.pickupLatitude = pickupLatitude;
+    }
+
+    public double getPickupLongitude() {
+        return pickupLongitude;
+    }
+
+    public void setPickupLongitude(double pickupLongitude) {
+        this.pickupLongitude = pickupLongitude;
+    }
+
+    public double getDropOffLatitude() {
+        return dropOffLatitude;
+    }
+
+    public void setDropOffLatitude(double dropOffLatitude) {
+        this.dropOffLatitude = dropOffLatitude;
+    }
+
+    public double getDropOffLongitude() {
+        return dropOffLongitude;
+    }
+
+    public void setDropOffLongitude(double dropOffLongitude) {
+        this.dropOffLongitude = dropOffLongitude;
+    }
+
+    public double getDestinationLatitude() {
+        return destinationLatitude;
+    }
+
+    public void setDestinationLatitude(double destinationLatitude) {
+        this.destinationLatitude = destinationLatitude;
+    }
+
+    public double getDestinationLongitude() {
+        return destinationLongitude;
+    }
+
+    public void setDestinationLongitude(double destinationLongitude) {
+        this.destinationLongitude = destinationLongitude;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getVehiclePlateNo() {
+        return vehiclePlateNo;
+    }
+
+    public void setVehiclePlateNo(String vehiclePlateNo) {
+        this.vehiclePlateNo = vehiclePlateNo;
+    }
 }
